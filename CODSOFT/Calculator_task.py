8e35a9f9a8e33c846651ce141b5217ff1297ad27
archana-agarwal-0507 <archanaagarwal23@ifheindia.org>
2025-04-05 22:13:11 +0530

    Update Calculator_task.py

--- CODSOFT/Calculator_task.py
+++ CODSOFT/Calculator_task.py
@@ -5,7 +5,7 @@ num1=float(input("enter first number:"))
 #Taking input of second number
 num2=float(input("enter second number:"))
 #choosing an operation to do 
-operation=input("select your choice of operation. Addition/Subtraction/Multiplication/Division").lower()
+operation=input("select your choice of operation. \nAddition\nSubtraction\nMultiplication\nDivision\n").lower()
 match operation:
     case "addition":
         result=num1+num2
